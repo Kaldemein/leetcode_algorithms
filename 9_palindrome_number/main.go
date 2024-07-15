53236255da435c5cf2083d037c6ed966b5b9e84d
Ruzel <ruzeldenf222@gmail.com>
2024-07-16 00:51:03 +0300

    9_palindrome_number realisation in golang

---
+++ 9_palindrome_number/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isPalindrome(x int) bool {
+	number := strconv.Itoa(x)
+	fmt.Printf("%s\n", number)
+	left := 0
+	right := len(number) - 1
+
+	for left <= right {
+		if number[left] != number[right] {
+			return false
+		}
+		left += 1
+		right -= 1
+	}
+	return true
+}
+
+func main() {
+	fmt.Printf("%t\n", isPalindrome(10))
+}
